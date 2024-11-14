799543c70099cdff300ff9de5eeeec265a2a064e
KC45jp <coltmikke@gmail.com>
2024-11-14 06:46:39 -0800

    Add: AbstractGUI Add: LogInGUI

---
+++ src/userData/userDefaultData.java
@@ -0,0 +1,16 @@
+package userData;
+
+public class userDefaultData {
+
+    String userId;
+    String password;
+
+    public userDefaultData(String userId, String password){
+        this.userId = userId;
+        this.password = password;
+    }
+
+
+
+
+}
